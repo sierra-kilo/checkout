78c664429426f2e585561a747ede9751cdc1c1da
Sierra Kilo <sierrakilo@Sierras-MBP.DFMNET01>
2017-11-07 11:49:35 -0800

    added boostrap card to sign up page

--- routes/auth.js
+++ routes/auth.js
@@ -6,20 +6,20 @@ module.exports = function(app, passport) {
     app.get('/signin', authController.signin);
     app.post('/signup', passport.authenticate('local-signup', {
             successRedirect: '/dashboard',
-            failureRedirect: '/signup'
+            failureRedirect: '/'
         }
     ));
     app.get('/dashboard', isLoggedIn, authController.dashboard);
     app.get('/logout', authController.logout);
     app.post('/signin', passport.authenticate('local-signin', {
             successRedirect: '/dashboard',
-            failureRedirect: '/signin'
+            failureRedirect: '/'
         }
     ));
 
     function isLoggedIn(req, res, next) {
         if (req.isAuthenticated())
             return next();
-        res.redirect('/signin');
+        res.redirect('/');
     }
 }
