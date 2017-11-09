532123fc670d9bf75380ac3bf04de4fb353c8d36
Sierra Kilo <sierrakilo@Sierras-MBP.DFMNET01>
2017-11-08 21:47:32 -0500

    Refactor server.js and reorder middleware

--- server.js
+++ server.js
@@ -3,8 +3,8 @@ var app = express();
 
 var port = process.env.PORT || 8080;
 
-var passport   = require('passport');
-var session    = require('express-session');
+var passport = require('passport');
+var session = require('express-session');
 var bodyParser = require('body-parser');
 
 var env = require('dotenv').load();
@@ -24,21 +24,9 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
 // For Passport
-
-app.use(session({ secret: 'keyboard cat',resave: true, saveUninitialized:true})); // session secret
+app.use(session({ secret: 'keyboard cat', resave: true, saveUninitialized: true })); // session secret
 app.use(passport.initialize());
 app.use(passport.session()); // persistent login sessions
-
-app.use(function(req, res, next) {
-    res.locals.user = req.user;
-    next()
-});
-
-
-// app.get('/', function(req, res) {
-//     res.send('Welcome to Passport with Sequelize');
-// });
-
 
 // -----------------------------------------------
 var htmlRoute = require('./routes/html.js')(app);
@@ -47,26 +35,23 @@ var htmlRoute = require('./routes/html.js')(app);
 var models = require("./models");
 
 //Sync Database
-models.sequelize.sync().then(function() {
-
+models.sequelize.sync().then(function () {
     console.log('Nice! Database looks fine')
-
-}).catch(function(err) {
-
+}).catch(function (err) {
     console.log(err, "Something went wrong with the Database Update!")
-
 });
-
-
-//Routes
-var authRoute = require('./routes/auth.js')(app, passport);
 
 //load passport strategies
 require('./config/passport/passport.js')(passport, models.user);
+app.use(function (req, res, next) {
+    res.locals.user = req.user;
+    next()
+});
+//Routes
+var authRoute = require('./routes/auth.js')(app, passport);
 
 
-
-app.listen(port, function(err) {
+app.listen(port, function (err) {
 
     if (!err)
         console.log("Site is live on PORT:", port);
